d388123e5e614619fedb1c70d6144dff0c0e8dc2
Naser786naser <nasera871@gmail.com>
2022-06-22 13:37:28 +0100

    First Commit

---
+++ mvnTraining/src/test/java/mvnTrainingPractice/LaunchBrowser.java
@@ -0,0 +1,32 @@
+package mvnTrainingPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchBrowser {
+	@Test
+	public void launchBrowser()
+	{
+		String DriverPath = System.getProperty("user.dir");
+
+		System.setProperty("webdriver.chrome.driver", DriverPath + "\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		System.out.println(driver.findElement(By.id("ctl00_mainContent_chk_SeniorCitizenDiscount")).isSelected());
+
+		driver.findElement(By.id("ctl00_mainContent_chk_SeniorCitizenDiscount")).click();
+		System.out.println(driver.findElement(By.id("ctl00_mainContent_chk_SeniorCitizenDiscount")).isSelected());
+
+		int checkBoxSize = driver.findElements(By.xpath("//input[@type='checkbox']")).size();
+		System.out.println(checkBoxSize);
+
+	}
+
+}
